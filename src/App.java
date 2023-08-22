b44ac4d44dc36fe28af14eafa012f7027dbf3a32
ouzyu <murahiro927@gmail.com>
2023-08-22 17:57:56 +0900

    [Finish]chapter-0

--- src/App.java
+++ src/App.java
@@ -1,5 +1,15 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        System.out.println("test word");
+        System.out.println("31 + 31の計算をします");
+        System.out.println(31 + 31);
+        System.out.println(35 - 10);
+        System.out.println(-5 * 2);
+        System.out.println(6 * 6 * 3.14);
+        System.out.println("こたえは" + 64);
+        int x;
+        x = 6;
+        System.out.println(x * x * 3.14);
     }
 }
