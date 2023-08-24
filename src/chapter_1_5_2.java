c116ade3b4dc5bd410d5098b8d3d7353d48ea0dc
ouzyu <murahiro927@gmail.com>
2023-08-24 17:36:09 +0900

    [Finish]chapter_1_5

---
+++ src/chapter_1_5_2.java
@@ -0,0 +1,8 @@
+public class chapter_1_5_2 {
+    public static void main(String[] args) throws Exception {
+        int a = 3;
+        int b = 5;
+        int c = a * b;
+        System.out.println("縦幅" + a + "横幅" + "の長方形の面積は、" + c);
+    }
+}
---
+++ src/chapter_1_5_3.java
@@ -0,0 +1,9 @@
+public class chapter_1_5_3 {
+    public static void main(String[] args) throws Exception {
+        boolean bool = true;
+        char character = '駆';
+        double pie = 3.14;
+        long num = 314159265853979L;
+        String message = "ミナトの攻撃！敵に15ポイントのダメージを与えた。";
+    }
+}
